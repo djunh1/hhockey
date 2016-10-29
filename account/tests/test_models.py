d679a0a9162d68fb1bbfadd63cdf65f8a1b8e3c6
djunh1 <djunh1@gmail.com>
2016-10-29 12:13:03 -0400

    Test coverage for model and profile.

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -8,7 +8,7 @@ class AppTesting(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(username='Eric_Seager', email='jacob@…', password='top_secret')
-        self.user.profile.league = 'COLLEGE'
+        #self.user.profile.league = 'COLLEGE'
 
     def tearDown(self):
         self.user.delete()
@@ -18,7 +18,12 @@ class AppTesting(TestCase):
         self.assertEqual(str(self.user), 'Eric_Seager')
 
     def testProfileIsCreated(self):
-        self.assertEqual(str(self.user.profile.league), 'COLLEGE')
+        self.assertEqual(hasattr(self.user, 'profile'), True)
+
+    def testProfileIsSaved(self):
+        self.user.profile.position = 'WING'
+        self.user.save()
+        self.assertEqual(str(self.user.profile.position), 'WING')
 
 
 
