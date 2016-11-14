c1f1963f5f6277d61976247e292aa60048298448
djunh1 <djunh1@gmail.com>
2016-11-13 21:37:32 -0500

    Add Oscar to project.  Extend and modify some templates

--- hhockey/urls.py
+++ hhockey/urls.py
@@ -8,12 +8,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from oscar.app import application as oscarApp
+from paypal.express.dashboard.app import application as paypal_application
 from staticContent import views as staticViews
 
 
+
 urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^$', staticViews.home, name='home_page'),
-    url(r'shop/', include(oscarApp.urls)),
     url(r'^about/$', staticViews.about, name='about_page'),
     url(r'^contact/$', staticViews.contact, name='contact_page'),
     url(r'^faq/$', staticViews.faq, name='faq_page'),
@@ -22,6 +24,14 @@ urlpatterns = [
     url(r'account/',  include('account.urls', namespace='account')),
     url(r'accounts/', include('allauth.urls')),
 
+    #paypal
+    url(r'^checkout/paypal/', include('paypal.express.urls')),
+    url(r'^dashboard/paypal/express/', include(paypal_application.urls)),
+
+    #oscar App
+    url(r'shop/', include(oscarApp.urls)),
+
+    #admin
     url(r'^admin/', admin.site.urls),
 ]
 
---
+++ oscarCustom/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OscarcustomConfig(AppConfig):
+    name = 'oscarCustom'
