b9fb17467b37c4a863160c6ab6ce3fe366f03c89
djunh1 <djunh1@gmail.com>
2017-04-05 17:18:35 -0400

    REMOVE user dob from model

--- hhockeyUser/models.py
+++ hhockeyUser/models.py
@@ -20,6 +20,5 @@ POSITION_CHOICES = (
 
 class User(CoreAbstractUser):
     username = models.CharField(max_length=45, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
     league = models.CharField(max_length=15, choices=LEAGUE_CHOICES, blank=True)
     position = models.CharField(max_length=8, choices=POSITION_CHOICES, blank=True)
