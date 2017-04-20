6bb04d7e3539c6d6f2691ee6b1269fef571e7adb
djunh1 <djunh1@gmail.com>
2017-04-20 17:48:00 -0400

    HHOC-7 - Fix rendering of Notification with invalid email.  Add exception handling and FAQ.

--- oscarCustom/checkout/facade.py
+++ oscarCustom/checkout/facade.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from oscar.apps.payment.exceptions import UnableToTakePayment, InvalidGatewayRequestError
+from oscar.apps.payment.exceptions import UnableToTakePayment, InvalidGatewayRequestError, GatewayError
 
 import stripe
 
@@ -15,6 +15,24 @@ class Facade(object):
     @staticmethod
     def get_error_message(error):
         return 'We encountered an error while communicating with the payment gateway.'
+
+    @staticmethod
+    def get_api_message(error):
+        return 'We were unable to connect to Stripe.  Please try again later.'
+
+    @staticmethod
+    def get_rate_error(error):
+        return 'Too many requests made to this API.  Please try again later.'
+
+    @staticmethod
+    def get_invalid_request_error(error):
+        return 'Invalid parameters were supplied to the Stripe API'
+
+    @staticmethod
+    def get_auth_error(error):
+        return 'We were unable to authenticate our system with Stripe.  Please try again later.'
+
+
 
     def charge(self,
                order_number,
@@ -39,5 +57,20 @@ class Facade(object):
 
         except stripe.error.StripeError as e:
             raise InvalidGatewayRequestError(self.get_error_message(e))
+
+        except stripe.error.APIConnectionError as e:
+            raise UnableToTakePayment(self.get_api_message(e))
+
+        except stripe.error.InvalidRequestError as e:
+            raise InvalidGatewayRequestError(self.get_invalid_request_error(e))
+
+        except stripe.error.RateLimitError as e:
+            raise GatewayError(self.get_rate_error(e))
+
+        except stripe.error.AuthenticationError as e:
+            raise GatewayError(self.get_auth_error(e))
+
+        except Exception as e:
+            raise UnableToTakePayment(self.get_error_message(e))
 
 
--- oscarCustom/customer/alerts/views.py
+++ oscarCustom/customer/alerts/views.py
@@ -1,6 +1,10 @@
 from oscar.apps.customer.alerts.views import ProductAlertListView as CoreProductAlertListView
-
+from oscar.apps.customer.alerts.views import ProductAlertCreateView as CoreProductAlertCreateView
 
 class ProductAlertListView(CoreProductAlertListView):
     template_name = 'oscar/customer/alerts/alert_list.html'
+
+
+class ProductAlertCreateView(CoreProductAlertCreateView):
+    template_name = 'oscar/customer/alerts/form.html'
 
