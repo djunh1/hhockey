ec82315ead7a02461a9bd365e6717c1f80d059da
djunh1 <djunh1@gmail.com>
2017-08-09 19:47:13 -0400

    Add Basic Comment system, no authentication needed

--- hhblog/views.py
+++ hhblog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -14,8 +14,19 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year)
-
-    return render(request, 'post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+        new_comment = False
+    return render(request, 'post/detail.html', {'post': post,
+                                                'comments': comments,
+                                                'comment_form': comment_form})
 
 
 def post_share(request, post_id):
